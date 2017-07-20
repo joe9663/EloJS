84dfa4b8f44a93da048935266722f0cde72b6a54
JosephW <JosephW>
2017-07-20 18:20:21 -0400

    adds step parameter to range function

--- ch04/rangeAndSum.js
+++ ch04/rangeAndSum.js
@@ -1,17 +1,29 @@
-var range = function (num1, num2) {
+var range = function (num1, num2, step) {
   rangeValues = [];
   if (num1 < num2 == true) {
     counter = num1;
     while (counter < (num2 + 1)) {
       rangeValues.push(counter);
-      counter += 1;
+      if (step > 0 == true) {
+        counter += step;
+      }
+      else if (step < 0 == true){
+        counter -= step;
+      }
+      else counter += 1;
     }
   }
   else {
     counter = num2;
     while (counter < (num1 + 1)) {
       rangeValues.push(counter);
-      counter += 1;
+      if (step > 0 == true) {
+        counter += step;
+      }
+      else if (step < 0 == true){
+        counter -= step;
+      }
+      else counter += 1;
     }
   }
   return rangeValues;
