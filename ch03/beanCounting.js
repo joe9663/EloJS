994c6ff5aaad75ad111738150d40dd60eae7991b
JosephW <JosephW>
2017-07-20 16:23:16 -0400

    create countChar function

--- ch03/beanCounting.js
+++ ch03/beanCounting.js
@@ -10,3 +10,16 @@ var countBs = function (word) {
   }
   return theBs;
 }
+
+var countChar = function (word, letter) {
+  var counter = 0;
+  var total = 0;
+  while (counter < word.length) {
+    if (word[counter] == letter) {
+      total +=1;
+      counter += 1;
+    }
+    else counter += 1;
+  }
+  return total;
+}
